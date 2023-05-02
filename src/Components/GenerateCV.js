d1af0b06e73b5e2470f127ec3f97accf58079765
Md. Samin Irtiza <samin.irtiza@bjitacademy.com>
2023-05-02 16:21:19 +0600

    Update Menu.js

--- src/Components/GenerateCV.js
+++ src/Components/GenerateCV.js
@@ -29,7 +29,7 @@ export const GenerateCV = ({ email, location, telephone, social }) => {
     pdf.text(telephone, 15, 92);
 
     const starterPositionX = 15;
-    let starterPositionY = 406;
+    let starterPositionY = 356;
     social.forEach((socialProp) => {
       pdf.textWithLink(socialProp.label, starterPositionX, starterPositionY, {
         url: socialProp.url,
